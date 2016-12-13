0bb22769bf7b37183183fa8f221893e5439e4034
Mark Hobson <markhobson@gmail.com>
2016-12-13 20:20:18 +0000

    Added OAuth endpoint to obtain access token

--- hello-lambda/handler.js
+++ hello-lambda/handler.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const https = require('https');
+
 const client = {
-	id: process.env.CLIENT_ID
+	id: process.env.CLIENT_ID,
+	secret: process.env.CLIENT_SECRET
 }
 
 module.exports.install = (event, context, callback) => {
@@ -21,6 +24,38 @@ module.exports.install = (event, context, callback) => {
 					<a href="https://slack.com/oauth/authorize?scope=bot&client_id=${client.id}">
 						<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/>
 					</a>
+				</body>
+			</html>`
+	});
+};
+
+module.exports.thanks = (event, context, callback) => {
+	const code = event.queryStringParameters.code;
+	
+	https.get(`https://slack.com/api/oauth.access?client_id=${client.id}&client_secret=${client.secret}&code=${code}`, response => {
+		var body = '';
+		response.on('data', chunk => body += chunk);
+		response.on('end', () => {
+			const jsonBody = JSON.parse(body);
+			const botAccessToken = jsonBody.bot.bot_access_token;
+			const teamId = jsonBody.team_id;
+			console.log(`team id = ${teamId}, bot access token = ${botAccessToken}`);
+		});
+	});
+	
+	callback(null, {
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'text/html'
+		},
+		body: `<!DOCTYPE html>
+			<html>
+				<head>
+					<title>hellobot</title>
+				</head>
+				<body>
+					<h1>hellobot</h1>
+					<p>Thanks and enjoy!</p>
 				</body>
 			</html>`
 	});
