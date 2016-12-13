10beff5547f1255ec57340fa0a87d5123fb4fb28
Mark Hobson <markhobson@gmail.com>
2016-12-13 11:20:35 +0000

    Updated client to invoke new lambda

--- client/client.js
+++ client/client.js
@@ -1,9 +1,9 @@
 var AWS = require('aws-sdk');
 
 // workaround: https://github.com/aws/aws-sdk-js/issues/1039
-AWS.config.update({region: 'us-west-2'});
+AWS.config.update({region: 'eu-west-1'});
 
 var lambda = new AWS.Lambda();
-lambda.invoke({FunctionName: 'hello-world-function'}, function(error, data) {
+lambda.invoke({FunctionName: 'aws-nodejs-dev-hello'}, function(error, data) {
 	console.log(error ? error : data);
 });
