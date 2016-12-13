ac82dab4803f682ad363816c945b8808e95565a8
Mark Hobson <markhobson@gmail.com>
2016-12-13 19:42:14 +0000

    Added install function for Slack Button

--- hello-lambda/handler.js
+++ hello-lambda/handler.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const client = {
+	id: process.env.CLIENT_ID
+}
+
+module.exports.install = (event, context, callback) => {
+	callback(null, {
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'text/html'
+		},
+		body: `<!DOCTYPE html>
+			<html>
+				<head>
+					<title>hellobot</title>
+				</head>
+				<body>
+					<h1>hellobot</h1>
+					<p>Click the button to add @hellobot to Slack!</p>
+					<a href="https://slack.com/oauth/authorize?scope=bot&client_id=${client.id}">
+						<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/>
+					</a>
+				</body>
+			</html>`
+	});
+};
+
 module.exports.hello = (event, context, callback) => {
 	console.log(JSON.stringify(event));
 	
