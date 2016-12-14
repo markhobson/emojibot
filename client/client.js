08a55319d6d888c6bbce631319ca235458923c15
Mark Hobson <markhobson@gmail.com>
2016-12-14 13:12:42 +0000

    Removed client module as it's replaced by Slack

--- client/client.js
+++
@@ -1,9 +0,0 @@
-var AWS = require('aws-sdk');
-
-// workaround: https://github.com/aws/aws-sdk-js/issues/1039
-AWS.config.update({region: 'eu-west-1'});
-
-var lambda = new AWS.Lambda();
-lambda.invoke({FunctionName: 'hello-dev-hello'}, function(error, data) {
-	console.log(error ? error : data);
-});
