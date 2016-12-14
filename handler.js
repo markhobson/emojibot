eec082955e0ac66708973ef9b0e5dc02d69f102c
Mark Hobson <markhobson@gmail.com>
2016-12-14 21:47:57 +0000

    Singularize input words

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@ const WebClient = require('@slack/client').WebClient;
 const OAuth = require('./oauth.js');
 const Templates = require('./templates.js');
 const emoji = require('./emoji.js');
+const pluralize = require('pluralize');
 
 const client = {
 	id: process.env.CLIENT_ID,
@@ -84,6 +85,7 @@ const handleEvent = (event, token) => {
 const handleMessage = text => {
 	const replies = text.match(/\w{2,}/g)
 		.map(word => word.toLowerCase())
+		.map(pluralize.singular)
 		.filter(word => emoji[word])
 		.map(word => `:${word}:`);
 	
