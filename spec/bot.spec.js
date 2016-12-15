84e1ba523b32184b87f04a421e131e025b15bb75
Mark Hobson <markhobson@gmail.com>
2016-12-15 14:39:51 +0000

    Don't respond in channels when nothing to add

--- spec/bot.spec.js
+++ spec/bot.spec.js
@@ -13,93 +13,109 @@ describe('Bot', () => {
 		bot = new Bot(web);
 	});
 	
-	it('should respond with matching emoji', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
+	describe('when in channel', () => {
+		const channel = 'C1234567890';
 		
-		bot.process({text: 'pig', channel: 'channel'}, 'token');
-		
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', ':pig:');
+		it('should not respond when no matches', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'foo', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).not.toHaveBeenCalled();
+		});
 	});
 	
-	it('should respond with matching emoji ignoring case', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
+	describe('when in direct message', () => {
+		const channel = 'D1234567890';
 		
-		bot.process({text: 'PIG', channel: 'channel'}, 'token');
+		it('should respond with matching emoji', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'pig', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, ':pig:');
+		});
 		
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', ':pig:');
-	});
-	
-	it('should respond with emoji for plurals', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
-
-		bot.process({text: 'pigs', channel: 'channel'}, 'token');
-
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', ':pig:');
-	});
-
-	it('should respond with emoji for singulars', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
-
-		bot.process({text: 'eye', channel: 'channel'}, 'token');
-
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', ':eyes:');
-	});
-	
-	it('should respond with emoji for matching word', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
-
-		bot.process({text: 'foo pig foo', channel: 'channel'}, 'token');
-
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', ':pig:');
-	});
-	
-	it('should respond with random emoji for matching word', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
-
-		bot.process({text: 'dog', channel: 'channel'}, 'token');
-
-		expect(web.chat.postMessage)
-			.toHaveBeenCalledWith('channel', jasmine.stringMatching(/(:dog:|:feet:|:poodle:|:wolf:)/));
-	});
-	
-	it('should respond with emoji for a random matching word', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
-
-		bot.process({text: 'horse pig wolf', channel: 'channel'}, 'token');
-
-		expect(web.chat.postMessage)
-			.toHaveBeenCalledWith('channel', jasmine.stringMatching(/(:horse:|:pig:|:wolf:)/));
-	});
-	
-	it('should respond with emoji for synonyms', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
+		it('should respond with matching emoji ignoring case', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'PIG', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, ':pig:');
+		});
 		
-		bot.process({text: 'woof', channel: 'channel'}, 'token');
+		it('should respond with emoji for plurals', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'pigs', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, ':pig:');
+		});
 		
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', ':dog:');
-	});
-	
-	it('should respond with message for single letter words', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
+		it('should respond with emoji for singulars', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'eye', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, ':eyes:');
+		});
 		
-		bot.process({text: 'a', channel: 'channel'}, 'token');
+		it('should respond with emoji for matching word', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'foo pig foo', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, ':pig:');
+		});
 		
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', 'I have nothing.');
-	});
-	
-	it('should respond with message for common words', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
+		it('should respond with random emoji for matching word', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'dog', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage)
+				.toHaveBeenCalledWith(channel, jasmine.stringMatching(/(:dog:|:feet:|:poodle:|:wolf:)/));
+		});
 		
-		bot.process({text: 'like', channel: 'channel'}, 'token');
+		it('should respond with emoji for a random matching word', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'horse pig wolf', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage)
+				.toHaveBeenCalledWith(channel, jasmine.stringMatching(/(:horse:|:pig:|:wolf:)/));
+		});
 		
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', 'I have nothing.');
-	});
-	
-	it('should respond with message when no matches', () => {
-		spyOn(web.chat, 'postMessage').and.callThrough();
+		it('should respond with emoji for synonyms', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'woof', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, ':dog:');
+		});
 		
-		bot.process({text: 'foo', channel: 'channel'}, 'token');
+		it('should respond with message for single letter words', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'a', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, 'I have nothing.');
+		});
 		
-		expect(web.chat.postMessage).toHaveBeenCalledWith('channel', 'I have nothing.');
+		it('should respond with message for common words', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'like', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, 'I have nothing.');
+		});
+		
+		it('should respond with message when no matches', () => {
+			spyOn(web.chat, 'postMessage').and.callThrough();
+			
+			bot.process({text: 'foo', channel: channel}, 'token');
+			
+			expect(web.chat.postMessage).toHaveBeenCalledWith(channel, 'I have nothing.');
+		});
 	});
 });
--- src/bot.js
+++ src/bot.js
@@ -32,10 +32,14 @@ Bot.prototype.process = function(event) {
 		.filter(emojis => emojis !== undefined)
 		.reduce((array, next) => array.concat(next), []);
 	
-	const reply = replies[Math.floor(Math.random() * replies.length)] || 'I have nothing.';
-	
-	this.web.chat.postMessage(event.channel, reply)
-		.catch(error => console.log(`Error posting Slack message: ${error}`));
+	const direct = event.channel.startsWith('D');
+	const defaultReply = direct ? 'I have nothing.' : undefined;
+	const reply = replies[Math.floor(Math.random() * replies.length)] || defaultReply;
+
+	if (reply) {
+		this.web.chat.postMessage(event.channel, reply)
+			.catch(error => console.log(`Error posting Slack message: ${error}`));
+	}
 };
 
 module.exports = Bot;
